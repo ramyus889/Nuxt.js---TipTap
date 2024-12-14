c1c442c36fd8ca8b48e9a36f3ea55499658122cb
ramyus889 <ramyus889.com>
2024-12-14 15:26:04 +0300

    add

--- app.vue
+++ app.vue
@@ -2,8 +2,14 @@
   <div class="mt-10 ms-10">
     <div class="flex gap-4 items-center">
       <Icon name="devicon:nuxtjs" class="text-4xl" />
-      <div class="text-3xl">Frontend</div>
+      <div class="text-3xl UserBorder">Frontend</div>
       <Icon name="svg-spinners:blocks-shuffle-3" class="text-4xl" />
+      <Button label="Button" icon="pi pi-home" />
+      <UseButton
+        label="UseButton"
+        motionClick="motion-preset-pulse"
+        icon="pi pi-chevron-right"
+      />
     </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
+import Aura from "@primevue/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@primevue/nuxt-module"],
+  css: ["primeicons/primeicons.css", "./css/style.css"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
   icon: {
     serverBundle: {
       collections: ["uil", "devicon", "svg-spinners"],
