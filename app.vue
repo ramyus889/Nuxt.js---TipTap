af21878993685e0c2ad47ec7a8fac7783b663a81
ramyus889 <ramyus889.com>
2024-12-14 13:26:40 +0300

    add

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="mt-10 ms-10">
+    <div class="flex gap-4 items-center">
+      <Icon name="uil:github" class="text-4xl" />
+      <div class="text-3xl">Frontend</div>
+    </div>
+  </div>
+</template>
