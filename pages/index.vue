bc283999bbe1f523152bbf3fbed910f6cc9e0578
ramyus889 <ramyus889.com>
2024-12-14 18:05:50 +0300

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import IndexCard from "./pageComponents/indexComponents/IndexCard.vue";
-import IndexMain from "./pageComponents/indexComponents/IndexMain.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="flex place-content-start">
-    <div class="flex flex-col items-center gap-5">
-      <IndexMain />
-      <IndexCard />
-    </div>
+    <div class="">Frontend</div>
   </div>
 </template>
--- pages/pageComponents/indexComponents/IndexCard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="">
-    <div class="flex items-center gap-4">
-      <div class="text-3xl UserBorder">IndexCard</div>
-      <Icon name="uil:github" class="text-4xl" />
-    </div>
-  </div>
-</template>
