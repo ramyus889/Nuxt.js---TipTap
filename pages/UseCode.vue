82bf0e97ee68775cccc8d714bdd8b1675a86ddf4
ramyus889 <ramyus889.com>
2024-12-20 10:47:47 +0300

    add

--- pages/UseCode.vue
+++ pages/UseCode.vue
@@ -11,7 +11,34 @@ export default {
   <div class="">
     <v-btn>
       Code
-      <v-tooltip activator="parent" location="start">Tooltip</v-tooltip>
+      <v-tooltip activator="parent" location="start">
+        <v-card width="250" class="p-0">
+          <v-card-title> Login </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field v-model="login" label="Login" />
+              <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="loginUser">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-tooltip>
     </v-btn>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const login = ref("");
+const password = ref("");
+const loginUser = () => {
+  console.log(login.value, password.value);
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const counter = ref(0);
-const color = ref("");
+const color = ref("#3F51B5");
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -11,23 +10,49 @@ function getRandomColor() {
   return color;
 }
 function handleClick() {
-  counter.value++;
   color.value = getRandomColor();
 }
 </script>
 <template>
-  <div
-    class="p-4 max-w-[400px] flex place-content-center rounded-xl transition delay-150 duration-300"
-    :style="{ backgroundColor: color }"
-  >
+  <div class="inline-flex flex-col gap-5">
     <v-btn
-      prepend-icon="mdi-thumb-up"
+      prepend-icon="mdi-palette"
       variant="outlined"
       size="x-large"
       rounded="lg"
       @click="handleClick"
     >
-      {{ counter }}
+      Change color
     </v-btn>
+    <div class="w-full">
+      <div
+        class="max-w-[400px] px-5 rounded-xl transition duration-300"
+        :style="{ backgroundColor: color }"
+      >
+        <div class="flex flex-col items-center gap-5 py-5">
+          <v-img
+            class="w-[300px] rounded-xl"
+            src="/img/Code/frontend.jpg"
+            alt="random image"
+          />
+          <div class="">
+            <v-card-title class="text-2xl text-black">
+              {{ color }}
+            </v-card-title>
+            <v-card-text>
+              <!-- Removed paragraph text -->
+            </v-card-text>
+            <div class="flex flex-col gap-5">
+              <v-btn variant="outlined" size="large" rounded="lg"
+                >Learn more</v-btn
+              >
+              <v-btn variant="elevated" size="large" rounded="lg" color="blue"
+                >Buy now</v-btn
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
