9293eac9f865f5782eb68fadff7ad79685ff2b5f
ramyus889 <ramyus889.com>
2024-12-22 16:38:40 +0300

    Обновлено расположение мета-тега описания и удалены лишние элементы из компонента Head для улучшения структуры документа

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,25 +2,19 @@
 useHead({
   title: "Nuxt.js",
   meta: [
-    { name: "description", content: "Nuxt 3 Frontend Development" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "description", content: "Nuxt 3 Frontend Development" },
   ],
-  link: [
-    { rel: "icon", type: "image/x-icon", href: "/img/Code/frontend.jpg" },
-    { rel: "shortcut icon", href: "/img/Code/frontend.jpg" },
-  ],
+  link: [{ rel: "icon", type: "image/x-icon", href: "/img/Code/frontend.jpg" }],
 });
 </script>
 <template>
   <v-app>
     <v-main>
-      <Head>
-        <Meta name="yandex-verification" content="30dd311c3ef215f7" />
-        <UseHeader />
-        <div class="mt-[100px] sm:mx-[50px] mx-[30px]">
-          <slot />
-        </div>
-      </Head>
+      <UseHeader />
+      <div class="mt-[100px] sm:mx-[50px] mx-[30px]">
+        <slot />
+      </div>
     </v-main>
   </v-app>
 </template>
