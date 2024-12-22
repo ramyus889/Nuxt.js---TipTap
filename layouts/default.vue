dd8dc77b294de7e65e301db18b757a737d9e1128
ramyus889 <ramyus889.com>
2024-12-22 16:17:13 +0300

    Добавлено описание в мета-теги и обновлены ссылки на иконки в layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,14 @@
 useHead({
   title: "Nuxt.js",
   meta: [
+    { name: "description", content: "Nuxt 3 Frontend Development" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     { name: "yandex-verification", content: "30dd311c3ef215f7" },
-    { name: "description", content: "Nuxt 3 Frontend Development" },
   ],
-  link: [{ rel: "icon", type: "image/x-icon", href: "/img/Code/frontend.jpg" }],
+  link: [
+    { rel: "icon", type: "image/x-icon", href: "/img/Code/frontend.jpg" },
+    { rel: "shortcut icon", href: "/img/Code/frontend.jpg" },
+  ],
 });
 </script>
 <template>
